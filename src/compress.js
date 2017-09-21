435743bb27ed87af3530b617a2f885fc01635f40
Eugen Istoc <eugenistoc@gmail.com>
2017-09-21 00:29:47 -0400

    update

--- src/compress.js
+++ src/compress.js
@@ -30,68 +30,91 @@ class CompressPlugin {
     this.test = options.test || options.regExp;
     this.threshold = options.threshold || 0;
     this.minRatio = options.minRatio || 0.8;
-    this.deleteOriginalAssets = options.deleteOriginalAssets || false;
+    this.deleteOriginalAssets =
+      options.deleteOriginalAssets || false;
   }
 
   apply(compiler) {
-    compiler.plugin('compilation', compilation => {
-      compilation.plugin('html-webpack-plugin-after-html-processing', (htmlPluginData, callback) => {
-        const assets = compilation.assets;
+    compiler.plugin('compilation', (compilation) => {
+      compilation.plugin(
+        'html-webpack-plugin-after-html-processing',
+        (htmlPluginData, callback) => {
+          const assets = compilation.assets;
 
-        async.forEach(Object.keys(assets), (file, cb) => {
-          if (Array.isArray(this.test)) {
-            if (this.test.every(t => !t.test(file))) {
-              return cb();
-            }
-          } else if (this.test && !this.test.test(file)) {
-            return cb();
-          }
-          const asset = assets[file];
-          let content = asset.source();
+          async.forEach(
+            Object.keys(assets),
+            (file, cb) => {
+              if (Array.isArray(this.test)) {
+                if (this.test.every(t => !t.test(file))) {
+                  return cb();
+                }
+              } else if (
+                this.test &&
+                !this.test.test(file)
+              ) {
+                return cb();
+              }
+              const asset = assets[file];
+              let content = asset.source();
 
-          if (!Buffer.isBuffer(content)) {
-            content = new Buffer(content, 'utf-8');
-          }
+              if (!Buffer.isBuffer(content)) {
+                content = new Buffer(content, 'utf-8');
+              }
 
-          const originalSize = content.length;
+              const originalSize = content.length;
 
-          if (originalSize < this.threshold) {
-            return cb();
-          }
+              if (originalSize < this.threshold) {
+                return cb();
+              }
 
-          this.algorithm(content, this.compressionOptions, (err, result) => {
-            if (err) {
-              return cb(err);
-            }
+              this.algorithm(
+                content,
+                this.compressionOptions,
+                (err, result) => {
+                  if (err) {
+                    return cb(err);
+                  }
 
-            if (result.length / originalSize > this.minRatio) {
-              return cb();
-            }
+                  if (
+                    result.length / originalSize >
+                    this.minRatio
+                  ) {
+                    return cb();
+                  }
 
-            const parse = url.parse(file);
-            const sub = {
-              file,
-              path: parse.pathname,
-              query: parse.query || '',
-            };
+                  const parse = url.parse(file);
+                  const sub = {
+                    file,
+                    path: parse.pathname,
+                    query: parse.query || '',
+                  };
 
-            let newFile = this.asset.replace(/\[(file|path|query)\]/g, (p0, p1) => sub[p1]);
+                  let newFile = this.asset.replace(
+                    /\[(file|path|query)\]/g,
+                    (p0, p1) => sub[p1],
+                  );
 
-            if (typeof this.filename === 'function') {
-              newFile = this.filename(newFile);
-            }
-            assets[newFile] = new RawSource(result);
-            if (this.deleteOriginalAssets) {
-              delete assets[file];
-            }
+                  if (typeof this.filename === 'function') {
+                    newFile = this.filename(newFile);
+                  }
+                  assets[newFile] = new RawSource(result);
+                  if (this.deleteOriginalAssets) {
+                    delete assets[file];
+                  }
 
-            htmlPluginData.html = htmlPluginData.html.replace(file, newFile);
-            cb();
-          });
-        }, callback);
-      });
-    })
-
+                  htmlPluginData.html = htmlPluginData.html.replace(
+                    file,
+                    newFile,
+                  );
+                  cb();
+                },
+              );
+            },
+            callback,
+          );
+        },
+      );
+    });
   }
 }
 
